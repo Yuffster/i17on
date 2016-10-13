c93ddfb87842f1b9cd95ba2889b054ebb55fb120
Michelle Steigerwalt <msteigerwalt@gmail.com>
2016-10-13 14:05:04 -0400

    Version bump; chmod +x on executable.

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup, find_packages
 
+VERSION = '0.1.1'
+
 config = {
     'description': "An intranationalization engine for dynamic Markdown documents.",
     'author': "Michelle Steigerwalt",
+    'author_email': "msteigerwalt@gmail.com",
     'url': 'http://i17on.com',
-    'version': '0.1',
+    'version': VERSION,
     'packages': find_packages(exclude=["tests"]),
     'entry_points': {
         "console_scripts": [
-            'i17on = i17on.__main__'
+            'i17on = i17on.__main__:main'
         ]
     },
     'name': 'i17on',
